181e82a811e1fcbc6fdfcff1c3e4e424c81f566c
medlon <103527833+medlon@users.noreply.github.com>
2023-03-20 17:42:11 +0200

    Structured the downloaded and final combined videos sepately

--- videos/mergevideosTerminalArguments.js
+++ videos/mergevideosTerminalArguments.js
@@ -6,13 +6,38 @@ const { exec } = require('child_process');
 // Get the arguments passed to the program
 const args = process.argv.slice(2);
 
+console.log('1');
+
+//check if 2 or more arguments where provided - if not terminate the program below
+if (args.length < 2 ) {
+  console.log("You should provide 2 or more file arguments");
+  process.exit();
+}
+
+console.log('2');
+
+//check if 2 or more arguments where provided - if not terminate the program above
 // Create a list.txt file and write the arguments to it
 const listFileName = 'list.txt';
 const fileContents = args.map(arg => `file ${arg}`).join('\n');
 fs.writeFileSync(listFileName, fileContents);
 
+console.log('3');
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+//Attempt to add finalMergedVideo.mp4 name check
+let mergedVideoName = '../combinedVideosFolder/finalMergedVideo1.mp4';
+let count = 1;
+while (fs.existsSync(mergedVideoName)) {
+  count++;
+  mergedVideoName = `../combinedVideosFolder/finalMergedVideo${count}.mp4`;
+}
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+console.log('4');
+
 // Concatenate the input files using FFmpeg
-const command = `ffmpeg -safe 0 -f concat -i ${listFileName} -c copy finalMergedVideo.mp4`;
+const command = `ffmpeg -safe 0 -f concat -i ${listFileName} -c copy ${mergedVideoName}`;
 exec(command, (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
@@ -24,3 +49,5 @@ exec(command, (error, stdout, stderr) => {
   // Delete the list.txt file
   fs.unlinkSync(listFileName);
 });
+
+console.log('5');
